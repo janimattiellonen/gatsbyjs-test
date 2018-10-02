6e6b38f0d39b2092905cd6f02c074a23a862ecf2
Janimatti Ellonen <janimatti.ellonen@gmail.com>
2018-10-02 18:04:14 +0300

    Changed Site title to "Home of Janimatti Ellonen" for now Removed some test text and links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Home of Janimatti Ellonen',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,17 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 export default ({data}) => {
-
-  console.log("sss: " + JSON.stringify(data.allMarkdownRemark, null, 2));
   return (
     <Layout>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
+      <h1>Hi people!</h1>
+      <p>Welcome to my humble blog.</p>
+      <p><Link to="/page-2/">Go to page 2</Link></p>
 
-      {data.allMarkdownRemark.edges.map((item, i) => (
-        <p key={i}><Link to={item.node.frontmatter.path}>{item.node.frontmatter.title}</Link></p>
-      ))}
+      <ul>
+        {data.allMarkdownRemark.edges.map((item, i) => (
+          <li key={i}><Link to={item.node.frontmatter.path}>{item.node.frontmatter.title}</Link></li>
+        ))}
+      </ul>
     </Layout>
   );
 }
